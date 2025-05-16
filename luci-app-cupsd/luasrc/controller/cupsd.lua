96543f269baf9e23f8b85a6b33d3c07410af16e0
Pdboy Sir <63764630+sirpdboy@users.noreply.github.com>
2025-05-16 13:11:44 +0800

    Update cupsd.lua

--- luci-app-cupsd/luasrc/controller/cupsd.lua
+++ luci-app-cupsd/luasrc/controller/cupsd.lua
@@ -10,6 +10,7 @@ function index()
 
 	entry({"admin", "services", "cupsd"},alias("admin", "services", "cupsd","basic"),_("CUPS打印服务器"),60)
 	entry({"admin", "services", "cupsd","basic"}, cbi("cupsd/basic"),_("设置"),10).leaf = true
+	
 	entry({"admin", "services", "cupsd","advanced"}, cbi("cupsd/advanced"),_("高级"),20).leaf = true
 	entry({"admin", "services", "cupsd_status"}, call("act_status"))
 end
